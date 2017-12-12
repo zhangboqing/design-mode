a8427866411a5c4a5f9a2ae2657e9e538a724ec2
zhangboqing <zhangboqing@beiquan.org>
2017-12-12 17:15:37 +0800

    项目初始化妆台

---
+++ src/main/java/com/zbq/DesignmodeApplication.java
@@ -0,0 +1,12 @@
+package com.zbq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignmodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignmodeApplication.class, args);
+	}
+}
