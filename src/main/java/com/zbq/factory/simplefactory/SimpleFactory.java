d80ebe3417f933b7f5877856a38f408d0ff8b134
zhangboqing <zhangboqing@beiquan.org>
2017-12-13 10:02:33 +0800

    工厂模式

---
+++ src/main/java/com/zbq/factory/simplefactory/SimpleFactory.java
@@ -0,0 +1,21 @@
+package com.zbq.factory.simplefactory;
+
+/**
+ * @author zhangboqing
+ * @date 2017/12/12
+ * 简单工厂
+ */
+public class SimpleFactory {
+
+    public static Object createObject() {
+
+        return null;
+    }
+
+    public static Object createObject(Integer type) {
+
+
+        return null;
+    }
+
+}
