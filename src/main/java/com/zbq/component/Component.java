47948c8962510321879bd280c401d1d3fc5b5138
zhangboqing <zhangboqing@beiquan.org>
2018-05-27 21:35:23 +0800

    组合模式

---
+++ src/main/java/com/zbq/component/Component.java
@@ -0,0 +1,11 @@
+package com.zbq.component;
+
+/**
+ * 抽象构件
+ */
+public abstract class Component {
+     //个体和整体都具有的共享  
+     public void doSomething(){  
+             //编写业务逻辑  
+     }  
+} 
---
+++ src/main/java/com/zbq/component/Leaf.java
@@ -0,0 +1,15 @@
+package com.zbq.component;
+
+/**
+ * 树叶构件
+ * 树叶节点是没有子下级对象的对象，定义参加组合的原始对象行为。
+ */
+public class Leaf extends Component {  
+     /* 
+      *  
+可以覆写父类方法 
+      * public void doSomething(){ 
+      *  
+      * } 
+      */  
+}  
